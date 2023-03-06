d1c702be927ebca0e41bd99718b5308bd26e6adf
Halla Smith <me@hcssmith.com>
2023-03-06 00:10:17 +0000

    Added toolbox to user registry

--- home.nix
+++ home.nix
@@ -2,4 +2,17 @@
 
 {
   programs.home-manager.enable = true;
+  nix.registry = {
+    toolbox = {
+      from = {
+        id = "toolbox";
+        type = "indirect";
+      };
+      to = {
+        owner = "hcssmith";
+        repo = "toolbox";
+        type = "github";
+      };
+    };
+  };
 }
